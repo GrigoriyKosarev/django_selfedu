c7e2f46c93ca4e6b65fe6630352abbc884bd88ce
Kosarev Grigoriy <grigoriykosarev@gmail.com>
2023-10-24 16:20:40 +0300

    test

--- sitewomen/women/views.py
+++ sitewomen/women/views.py
@@ -7,5 +7,14 @@ def index(request):
     return HttpResponse("Page app women")
 
 
-def categories(request):
-    return HttpResponse("<h1>Page app categories</h1>")
+def categories(request, cat_id):
+    return HttpResponse(f"<h1>Page app categories</h1><p>id: {cat_id}</p>")
+
+
+def categories_by_slug(request, cat_slug):
+    return HttpResponse(f"<h1>Page app categories</h1><p>slug: {cat_slug}</p>")
+
+
+def archive(request, year):
+    return HttpResponse(f"<h1>Year</h1><p>year: {year}</p>")
+
